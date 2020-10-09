e4c7018491b7334e7ea4d39c384348edeee84231
vimal vasava <vimal.vasava@sooryen.com>
2020-10-09 17:26:53 +0530

    my first gradle commit

---
+++ src/main/java/springboot_pro/ServletInitializer.java
@@ -0,0 +1,13 @@
+package springboot_pro;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ComApplication.class);
+	}
+
+}
